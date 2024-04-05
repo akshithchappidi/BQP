bb12871704ea16ad99e1fb3a0b1098e6e6c37fb6
AkshithChowdary <151607595+AkshithChowdary@users.noreply.github.com>
2024-04-05 18:14:13 +0530

    eslint errors

--- Components/AboutUs.tsx
+++ Components/AboutUs.tsx
@@ -1,6 +1,5 @@
-import Image from 'next/image';
-
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 
 interface GradientTextProps {
   children: React.ReactNode;
@@ -17,50 +16,52 @@ const GradientText: React.FC<GradientTextProps> = ({ children, gradient }) => {
 
 const AboutComponent = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center md:mt-12">
-      <div className="md:w-1/2 p-4 pt-8 md:order-2">
-        <Image
-          src="/team.png"
-          alt="About Us"
-          width={800}
-          height={500}
-          className="w-full h-auto rounded-lg shadow-lg"
-        />
-      </div>
-      <div className="md:w-1/2 p-16 rounded-lg z-4 text-blue md:order-1">
-        <div className="relative">
-          <div className="relative w-auto h-auto bg-gradient-to-r from-indigo-500 to-purple-700 rounded-lg shadow-lg mt-8 transform rotate-y-3">
-            <h2 className="text-4xl font-bold mb-2 text-white font-mono text-bold p-4 text-center justify-center">
-              About BosonQ Psi
-            </h2>
-          </div>
+    <div className="py-32" style={{ transform: "skewY(0deg)", background: "linear-gradient(to bottom, black, #24A2B2)" }}>
+      <div className="container mx-auto flex flex-col md:flex-row items-center">
+        <div className="md:w-1/2 p-4 pt-8 md:order-2">
+          <Image
+            src="/team.png"
+            alt="About Us"
+            width={800}
+            height={500}
+            className="w-full h-auto rounded-lg shadow-lg"
+          />
         </div>
-        <p className="text-3xl text-bold text-white font-extra-large font-highlight font-serif mt-8">
-          We are on a{' '}
-          <GradientText gradient="from-blue-400 to-indigo-600">
-            mission
-          </GradientText>{' '}
-          to make engineering simulations smarter, faster, and efficient to drive
-          sustainable innovations.
-        </p>
-        <br />
-        <p className="text-2xl text-white font-serif mt-4">
-            <GradientText gradient='from-purple-600 to-blue-600'>At BQP, we're leading the charge in advanced simulation platforms with
-          one mission: to revolutionize computational problem-solving.</GradientText>
-           <br />{' '}
+        <div className="md:w-1/2 p-16 rounded-lg z-4 text-blue md:order-1">
+          <div className="relative">
+            <div className="relative w-auto h-auto bg-gradient-to-r from-indigo-500 to-purple-700 rounded-lg shadow-lg mt-8 transform rotate-y-3">
+              <h2 className="text-4xl font-bold mb-2 text-white font-mono text-bold p-4 text-center justify-center">
+                About BosonQ Psi
+              </h2>
+            </div>
+          </div>
+          <p className="text-3xl text-bold text-white font-extra-large font-highlight font-serif mt-8">
+            We are on a{" "}
+            <GradientText gradient="from-blue-400 to-indigo-600">
+              mission
+            </GradientText>{" "}
+            to make engineering simulations smarter, faster, and efficient to drive
+            sustainable innovations.
+          </p>
           <br />
-          <GradientText gradient="from-blue-600 to-green-600">Our cutting-edge technology delivers faster and more affordable
-          solutions, </GradientText>
-         <GradientText gradient='from-green-600 to-yellow-400'>Textslashing months off development cycles and saving millions.
-          Integrated with quantum algorithms,</GradientText>
-           <br /> <br />
-          <GradientText gradient="from-yellow-400 to-red-600">
-            our platform operates on today's HPC frameworks and future quantum
-            computers, offering superior solutions. Our platform offers
-            unprecedented possibilities with engineering simulations,
-            particularly in the aerospace, manufacturing, and energy sectors.
-          </GradientText>
-        </p>
+          <p className="text-2xl text-white font-serif mt-4">
+            <GradientText gradient="from-purple-600 to-blue-600">At BQP, we&apos;re leading the charge in advanced simulation platforms with
+            one mission: to revolutionize computational problem-solving.</GradientText>
+            <br />{" "}
+            <br />
+            <GradientText gradient="from-blue-600 to-green-600">Our cutting-edge technology delivers faster and more affordable
+            solutions, </GradientText>
+            <GradientText gradient="from-green-600 to-yellow-400">slashing months off development cycles and saving millions.
+            Integrated with quantum algorithms,</GradientText>
+            <br /> <br />
+            <GradientText gradient="from-yellow-400 to-red-600">
+              our platform operates on today&apos;s HPC frameworks and future quantum
+              computers, offering superior solutions. Our platform offers
+              unprecedented possibilities with engineering simulations,
+              particularly in the aerospace, manufacturing, and energy sectors.
+            </GradientText>
+          </p>
+        </div>
       </div>
     </div>
   );
--- pages/career.tsx
+++ pages/career.tsx
@@ -2,21 +2,21 @@ import Image from "next/image";
 
 export default function Career() {
   return (
-    <div className="py-32" style={{ transform: 'skewY(0deg)', background: 'linear-gradient(to bottom, black, #24A2B2)' }}>
+    <div className="py-32" style={{ transform: "skewY(0deg)", background: "linear-gradient(to bottom, black, #24A2B2)" }}>
       <div className="container mx-auto flex flex-col md:flex-row items-center">
         <div className="md:w-1/2 p-4">
           <h2 className="text-3xl font-bold mb-4">Join Our Team</h2>
           <p className="text-lg mb-8">
-            At BosonQ Psi, we're on a mission to revolutionize the way computational problems are solved in the aerospace
+            At BosonQ Psi, we&apos;re on a mission to revolutionize the way computational problems are solved in the aerospace
             and advanced manufacturing sectors. Our team of passionate, talented individuals is at the heart of our
-            success, and we're always on the lookout for exceptional people to join our ranks.
+            success, and we&apos;re always on the lookout for exceptional people to join our ranks.
           </p>
           <p className="text-lg mb-8">
-            We value innovation, collaboration, and a commitment to excellence. If you're driven by curiosity, thrive in
-            a fast-paced environment, and are eager to push the boundaries of what's possible, we want you on our team.
+            We value innovation, collaboration, and a commitment to excellence. If you&apos;re driven by curiosity, thrive in
+            a fast-paced environment, and are eager to push the boundaries of what&apos;s possible, we want you on our team.
           </p>
           <p className="text-lg mb-8">
-            At BosonQ Psi, you'll have the opportunity to work on cutting-edge projects, collaborate with industry
+            At BosonQ Psi, you&apos;ll have the opportunity to work on cutting-edge projects, collaborate with industry
             leaders, and shape the future of computational problem-solving. We offer competitive compensation, excellent
             benefits, and a supportive, inclusive culture that fosters personal and professional growth.
           </p>

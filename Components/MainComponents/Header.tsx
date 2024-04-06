b176db36edcb9db3faf45def0b4ec2b8d2473307
AkshithChowdary <151607595+AkshithChowdary@users.noreply.github.com>
2024-04-06 15:26:49 +0530

    reponsive changes, addition in header

--- Components/MainComponents/Header.tsx
+++ Components/MainComponents/Header.tsx
@@ -1,25 +1,27 @@
-import { useState, useRef } from "react";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
 const Header = () => {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
-  const [isDashboardOpen, setDashboardOpen] = useState(false);
-  const [isServicesOpen, setServicesOpen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   const handleToggleMobileMenu = () => {
     setMobileMenuOpen(!isMobileMenuOpen);
   };
 
-  const handleToggleDashboard = () => {
-    setDashboardOpen(!isDashboardOpen);
-    if (isServicesOpen) setServicesOpen(false);
-  };
-
-  const handleToggleServices = () => {
-    setServicesOpen(!isServicesOpen);
-    if (isDashboardOpen) setDashboardOpen(false);
-  };
+  const isMobile = windowWidth <= 768;
 
   return (
     <div className="h-32">
@@ -29,44 +31,48 @@ const Header = () => {
             <Image src="/BQP_Logo.png" alt="logo-image" width={80} height={80} className="m-2" />
             <div>
               <h1 className="text-3xl font-bold text-gray-800">BosonQ Psi</h1>
-              <h3 className="text-lg text-black">
-                Enabling Simulations with Quantum Paradigms
-              </h3>
+              <h3 className="text-lg text-black">Enabling Simulations with Quantum Paradigms</h3>
             </div>
           </div>
-          <div className="md:hidden absolute top-0 right-0 m-4">
-            <button
-              type="button"
-              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="navbar-dropdown"
-              aria-expanded={isMobileMenuOpen ? 'true' : 'false'}
-              onClick={handleToggleMobileMenu}
-            >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 17 14"
+          {isMobile && (
+            <div className="absolute top-0 right-0 m-4">
+              <button
+                type="button"
+                className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                aria-controls="navbar-dropdown"
+                aria-expanded={isMobileMenuOpen ? 'true' : 'false'}
+                onClick={handleToggleMobileMenu}
               >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 1h15M1 7h15M1 13h15"
-                />
-              </svg>
-            </button>
-          </div>
+                <span className="sr-only">Open main menu</span>
+                <svg
+                  className="w-5 h-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 17 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 1h15M1 7h15M1 13h15"
+                  />
+                </svg>
+              </button>
+            </div>
+          )}
           <div
             className={`${
               isMobileMenuOpen ? 'block' : 'hidden'
-            } md:block md:w-auto`}
+            } ${isMobile ? 'w-full' : 'md:w-auto'}`}
             id="navbar-dropdown"
           >
-            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white bg-white">
+            <ul
+              className={`flex flex-col font-medium p-4 ${
+                isMobile ? 'mt-16' : 'md:mt-0'
+              } border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:border-0 md:bg-white bg-white`}
+            >
               {/* Your navigation items */}
               <li>
                 <Link href="/" legacyBehavior>
@@ -90,36 +96,22 @@ const Header = () => {
               </li>
             </ul>
           </div>
-          <div className="flex items-center space-x-4">
-            <Link
-              href="#"
-              aria-label="Twitter"
-              className="text-gray-500 hover:text-gray-800"
-            >
-              <Image src="/assets/Twitter.png" alt="Twitter" width={24} height={24} />
-            </Link>
-            <Link
-              href="#"
-              aria-label="Instagram"
-              className="text-gray-500 hover:text-gray-800"
-            >
-              <Image src="/assets/Instagram.png" alt="Instagram" width={24} height={24} />
-            </Link>
-            <Link
-              href="#"
-              aria-label="LinkedIn"
-              className="text-gray-500 hover:text-gray-800"
-            >
-              <Image src="/assets/LinkedIn.png" alt="LinkedIn" width={24} height={24} />
-            </Link>
-            <Link
-              href="#"
-              aria-label="YouTube"
-              className="text-gray-500 hover:text-gray-800"
-            >
-              <Image src="/assets/YouTube.png" alt="YouTube" width={24} height={24} />
-            </Link>
-          </div>
+          {!isMobile && (
+            <div className="flex items-center space-x-4">
+              <Link href="#" aria-label="Twitter" className="text-gray-500 hover:text-gray-800">
+                <Image src="/assets/Twitter.png" alt="Twitter" width={24} height={24} />
+              </Link>
+              <Link href="#" aria-label="Instagram" className="text-gray-500 hover:text-gray-800">
+                <Image src="/assets/Instagram.png" alt="Instagram" width={24} height={24} />
+              </Link>
+              <Link href="#" aria-label="LinkedIn" className="text-gray-500 hover:text-gray-800">
+                <Image src="/assets/LinkedIn.png" alt="LinkedIn" width={24} height={24} />
+              </Link>
+              <Link href="#" aria-label="YouTube" className="text-gray-500 hover:text-gray-800">
+                <Image src="/assets/YouTube.png" alt="YouTube" width={24} height={24} />
+              </Link>
+            </div>
+          )}
         </div>
       </nav>
     </div>
